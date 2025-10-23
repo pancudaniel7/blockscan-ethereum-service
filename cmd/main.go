c88bcc95c078ab6014eef23bf20eed7f69ba4fb0
Daniel Pancu <pancudaniel7@gmail.com>
2025-10-23 23:24:30 +0300

    feat(server) Implemented graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"log"
+	"sync"
+
+	"github.com/gofiber/fiber/v3"
 	"github.com/pancudaniel7/blockscan-ethereum-service/internal/infra"
 	"github.com/pancudaniel7/blockscan-ethereum-service/internal/pkg/applog"
 )
 
 var (
 	logger applog.AppLogger
+	server *fiber.App
 )
 
 func main() {
-	logger = applog.NewAppDefaultLogger()
 	if err := infra.LoadConfig(); err != nil {
-		logger.Fatal("failed to load config: %v", err.Error())
+		log.Fatal("Failed to load config: ", err)
 	}
+	logger = applog.NewAppDefaultLogger()
+
+	var wg sync.WaitGroup
+	server = infra.StartServer(logger, &wg)
+
+	callBack := func() error {
+		logger.Debug("Executing shutdown callback...")
+		return nil
+	}
+	infra.ShutdownServer(logger, &wg, server, callBack)
 }
