c826525b8f98a1b5528b9c1377cf3e2ecf233bd9
Daniel Pancu <pancudaniel7@gmail.com>
2025-12-30 23:24:21 +0200

    fix(applog): Fix wrapper logger source atribute value

--- internal/pkg/applog/slogger.go
+++ internal/pkg/applog/slogger.go
@@ -1,12 +1,14 @@
 package applog
 
 import (
-	"context"
-	"log/slog"
-	"os"
-	"strings"
+    "context"
+    "fmt"
+    "log/slog"
+    "os"
+    "runtime"
+    "strings"
 
-	"github.com/spf13/viper"
+    "github.com/spf13/viper"
 )
 
 // DefaultLogger wraps slog.logger and implements AppLogger.
@@ -16,36 +18,68 @@ type DefaultLogger struct {
 
 // NewAppDefaultLogger creates a new DefaultLogger with default options.
 func NewAppDefaultLogger() *DefaultLogger {
-	levelStr := viper.GetString("log.level")
-	level := parseLogLevel(levelStr)
-	return &DefaultLogger{
-		logger: slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: level, AddSource: true})),
-	}
+    levelStr := viper.GetString("log.level")
+    level := parseLogLevel(levelStr)
+    return &DefaultLogger{
+        logger: slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: level, AddSource: false})),
+    }
 }
 
 func (l *DefaultLogger) Info(msg string, args ...any) {
-	l.logger.Info(msg, args...)
+    src := callerSource(1)
+    if src != "" {
+        args = append([]any{"source", src}, args...)
+    }
+    l.logger.Info(msg, args...)
 }
 
 func (l *DefaultLogger) Warn(msg string, args ...any) {
-	l.logger.Warn(msg, args...)
+    src := callerSource(1)
+    if src != "" {
+        args = append([]any{"source", src}, args...)
+    }
+    l.logger.Warn(msg, args...)
 }
 
 func (l *DefaultLogger) Error(msg string, args ...any) {
-	l.logger.Error(msg, args...)
+    src := callerSource(1)
+    if src != "" {
+        args = append([]any{"source", src}, args...)
+    }
+    l.logger.Error(msg, args...)
 }
 
 func (l *DefaultLogger) Debug(msg string, args ...any) {
-	l.logger.Debug(msg, args...)
+    src := callerSource(1)
+    if src != "" {
+        args = append([]any{"source", src}, args...)
+    }
+    l.logger.Debug(msg, args...)
 }
 
 func (l *DefaultLogger) Trace(msg string, args ...any) {
-	l.logger.Log(context.Background(), slog.Level(-8), msg, args...)
+    src := callerSource(1)
+    if src != "" {
+        args = append([]any{"source", src}, args...)
+    }
+    l.logger.Log(context.Background(), slog.Level(-8), msg, args...)
 }
 
 func (l *DefaultLogger) Fatal(msg string, args ...any) {
-	l.logger.Error(msg, args...)
-	os.Exit(1)
+    src := callerSource(1)
+    if src != "" {
+        args = append([]any{"source", src}, args...)
+    }
+    l.logger.Error(msg, args...)
+    os.Exit(1)
+}
+
+func callerSource(skip int) string {
+    _, file, line, ok := runtime.Caller(skip + 1)
+    if !ok {
+        return ""
+    }
+    return fmt.Sprintf("%s:%d", file, line)
 }
 
 func parseLogLevel(s string) slog.Level {
