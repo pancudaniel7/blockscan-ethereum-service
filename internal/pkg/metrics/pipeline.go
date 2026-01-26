78634033e8aab3ead3a284ae526b5d86131982c5
Daniel Pancu <pancudaniel7@gmail.com>
2026-01-26 02:04:29 +0200

    chore(metrics): Created End To End Scan to Publish latency in ms

--- internal/pkg/metrics/pipeline.go
+++ internal/pkg/metrics/pipeline.go
@@ -1,31 +1,31 @@
 package metrics
 
 import (
-    "sync"
+	"sync"
 
-    "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 type PipelineMetrics struct {
-    EndToEndLatencyMS prometheus.Histogram
+	EndToEndLatencyMS prometheus.Histogram
 }
 
 var (
-    pipelineOnce sync.Once
-    pipeline     *PipelineMetrics
+	pipelineOnce sync.Once
+	pipeline     *PipelineMetrics
 )
 
 func Pipeline() *PipelineMetrics {
-    pipelineOnce.Do(func() {
-        r := Registerer()
-        pipeline = &PipelineMetrics{
-            EndToEndLatencyMS: promauto.With(r).NewHistogram(prometheus.HistogramOpts{
-                Name:    "pipeline_end_to_end_latency_ms",
-                Help:    "end-to-end latency from scan to publish+marker+ack (ms)",
-                Buckets: []float64{5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 120, 150, 200, 300},
-            }),
-        }
-    })
-    return pipeline
+	pipelineOnce.Do(func() {
+		r := Registerer()
+		pipeline = &PipelineMetrics{
+			EndToEndLatencyMS: promauto.With(r).NewHistogram(prometheus.HistogramOpts{
+				Name:    "pipeline_end_to_end_latency_ms",
+				Help:    "end-to-end latency from scan to publish+marker+ack (ms)",
+				Buckets: []float64{30, 35, 40, 45, 50, 60},
+			}),
+		}
+	})
+	return pipeline
 }
